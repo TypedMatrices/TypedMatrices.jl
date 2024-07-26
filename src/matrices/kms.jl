df5a88d55c051be255dafe323f3c2b8a2a7ce474
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-07-26 18:30:59 +0100

    feat(matrices): kms properties

--- src/matrices/kms.jl
+++ src/matrices/kms.jl
@@ -35,6 +35,9 @@ KMS{T}(n::Integer, rho::Number) where {T} = KMS{T}(n, convert(T, rho))
 
 # properties
 size(A::KMS) = (A.n, A.n)
+LinearAlgebra.ishermitian(::KMS) = true
+LinearAlgebra.isposdef(A::KMS) = A.n <= 1 || 0 < abs(A.rho) < 1 ? true : false
+LinearAlgebra.issymmetric(A::KMS) = A.n <= 1 && typeof(A.rho) <: Real ? true : false
 
 # functions
 @inline Base.@propagate_inbounds function getindex(A::KMS{T}, i::Integer, j::Integer) where {T}
