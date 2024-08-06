a287b903c3f996a46d9c2e34079b556b56d5623d
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-06 18:15:17 +0100

    test(core): metadata tests

---
+++ test/metadata.jl
@@ -0,0 +1,47 @@
+using Suppressor
+
+# list_properties
+props = list_properties()
+@test isa(props, Vector)
+@test all(isa.(props, Property))
+
+# check_propertie_types
+@test isnothing(TypedMatrices.check_propertie_types(PropertyTypes.Symmetric, PropertyTypes.Inverse))
+@test_throws ArgumentError TypedMatrices.check_propertie_types(Int)
+@test_throws ArgumentError TypedMatrices.check_propertie_types(PropertyTypes.Symmetric, Int)
+
+# check_properties_exists
+@test isnothing(TypedMatrices.check_properties_exists(Property(:symmetric), Property(:inverse)))
+@test_throws ArgumentError TypedMatrices.check_properties_exists(Property(:notexists))
+@test_throws ArgumentError TypedMatrices.check_properties_exists(Property(:symmetric), Property(:notexists))
+
+# property_types_to_properties
+@test TypedMatrices.property_types_to_properties(PropertyTypes.Symmetric, PropertyTypes.Inverse) == [Property(:symmetric), Property(:inverse)]
+@test_throws ArgumentError TypedMatrices.property_types_to_properties(Int)
+@test_throws ArgumentError TypedMatrices.property_types_to_properties(PropertyTypes.Symmetric, Int)
+
+# supress method overwritten warning
+@suppress begin
+    # @properties
+    @test isnothing(@properties Matrix [:symmetric, :inverse])
+    @test_throws ArgumentError @properties Matrix [:symmetric, :notexists]
+
+    # register_properties
+    @test isnothing(TypedMatrices.register_properties(Matrix, [Property(:symmetric), Property(:inverse)]))
+    @test isnothing(TypedMatrices.register_properties(Matrix, Property(:symmetric), Property(:inverse)))
+    @test isnothing(TypedMatrices.register_properties(Matrix, :symmetric, :inverse))
+    @test isnothing(TypedMatrices.register_properties(Matrix, [:symmetric, :inverse]))
+    @test isnothing(TypedMatrices.register_properties(Matrix, PropertyTypes.Symmetric, PropertyTypes.Inverse))
+    @test isnothing(TypedMatrices.register_properties(Matrix, [PropertyTypes.Symmetric, PropertyTypes.Inverse]))
+    @test isnothing(TypedMatrices.register_properties(Matrix, PropertyTypes.Symmetric(), PropertyTypes.Inverse()))
+    @test isnothing(TypedMatrices.register_properties(Matrix, [PropertyTypes.Symmetric(), PropertyTypes.Inverse()]))
+    @test_throws ArgumentError TypedMatrices.register_properties(Matrix, :notexists)
+    @test_throws ArgumentError TypedMatrices.register_properties(Matrix, :symmetric, :notexists)
+    @properties Matrix Property[]
+
+    # properties
+    @properties Matrix [:symmetric, :inverse]
+    @test properties(Matrix) == [Property(:symmetric), Property(:inverse)]
+    @test properties(Matrix(ones(1, 1))) == [Property(:symmetric), Property(:inverse)]
+    @properties Matrix Property[]
+end
