84cc105249937f0fcda040227c146be7a7355074
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-07 19:05:53 +0100

    test(matrices): refactor

--- test/matrices/binomial.jl
+++ test/matrices/binomial.jl
@@ -1,9 +1,3 @@
-# content
-@test Binomial(5) ≈ [1 4 6 4 1; 1 2 0 -2 -1; 1 0 -2 0 1; 1 -2 0 2 -1; 1 -4 6 -4 1]
-
-# eltype
-@test test_matrix_elements(Binomial{Int32}(5))
-
 # constructors
 @test allequal([
     Binomial(5),
@@ -12,3 +6,9 @@
 
 # linear algebra functions
 test_linearalgrbra_functions(Binomial.(1:5))
+
+# eltype
+@test test_matrix_elements(Binomial{Int32}(5))
+
+# content
+@test Binomial(5) ≈ [1 4 6 4 1; 1 2 0 -2 -1; 1 0 -2 0 1; 1 -2 0 2 -1; 1 -4 6 -4 1]
