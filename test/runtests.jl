733fea507eaccf1b701128ff5bc3de57c43c0245
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-07 13:19:29 +0100

    test(core): add test utils

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,25 @@ using LinearAlgebra
 
 const builtin_matrices = list_matrices(Group(:builtin))
 
+function test_matrix_elements(A::AbstractMatrix{T}) where {T}
+    return all(typeof(A[i]) == T for i = eachindex(A))
+end
+
+function test_linearalgrbra_functions(A::AbstractMatrix)
+    matrix = Matrix(A)
+    return all([
+        isdiag(A) == isdiag(matrix),
+        ishermitian(A) == ishermitian(matrix),
+        isposdef(A) == isposdef(matrix),
+        issymmetric(A) == issymmetric(matrix),
+        adjoint(A) ≈ adjoint(matrix),
+        transpose(A) ≈ transpose(matrix),
+        det(A) == det(matrix),
+        inv(A) ≈ inv(matrix),
+        eigvals(A) ≈ eigvals(matrix),
+    ])
+end
+
 @testset "TypedMatrices.jl" begin
     include("types.jl")
     include("metadata.jl")
