eddf7e6a83dcb340a669cdeef7bdd44d186886c0
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-08 12:06:58 +0100

    test(matrices): add circulant tests

---
+++ test/matrices/circulant.jl
@@ -0,0 +1,15 @@
+# constructors
+@test allequal([
+    Circulant(5),
+    Circulant{Int}(5),
+    Circulant([1:5;]),
+])
+
+# linear algebra functions
+test_linearalgrbra_functions(Circulant.(1:5))
+
+# eltype
+@test test_matrix_elements(Circulant{Int32}(5))
+
+# content
+@test Circulant(5) ≈ [1 2 3 4 5; 5 1 2 3 4; 4 5 1 2 3; 3 4 5 1 2; 2 3 4 5 1]
