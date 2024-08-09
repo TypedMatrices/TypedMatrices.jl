23d229b39d762b71e52b2d33a0e88567de100cde
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-09 17:27:23 +0100

    test(matrices): add inversehilbert tests

---
+++ test/matrices/inversehilbert.jl
@@ -0,0 +1,16 @@
+# constructors
+@test allequal([
+    InverseHilbert(5),
+    InverseHilbert{Int}(5),
+    InverseHilbert{Rational{Int}}(5),
+])
+
+# linear algebra functions
+run_test_linearalgrbra_functions(InverseHilbert.(1:5))
+@test test_linearalgrbra_functions(InverseHilbert{BigInt}(5))
+
+# eltype
+@test test_matrix_elements(InverseHilbert{Int32}(5))
+
+# content
+@test InverseHilbert(5) ≈ [25 -300 1050 -1400 630; -300 4800 -18900 26880 -12600; 1050 -18900 79380 -117600 56700; -1400 26880 -117600 179200 -88200; 630 -12600 56700 -88200 44100]
