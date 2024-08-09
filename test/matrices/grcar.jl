df7e7ce96fb0883d8d6235dfcdfc1602db50c6dc
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-09 15:45:37 +0100

    test(matrices): add grcar tests

---
+++ test/matrices/grcar.jl
@@ -0,0 +1,17 @@
+# constructors
+@test allequal([
+    Grcar(5),
+    Grcar(5, 3),
+    Grcar{Int}(5),
+    Grcar{Int}(5, 3),
+])
+
+# linear algebra functions
+test_linearalgrbra_functions(Grcar.(1:5))
+
+# eltype
+@test test_matrix_elements(Grcar{Int32}(5))
+
+# content
+@test Grcar(5) ≈ [1 1 1 1 0; -1 1 1 1 1; 0 -1 1 1 1; 0 0 -1 1 1; 0 0 0 -1 1]
+@test Grcar(5, 2) ≈ [1 1 1 0 0; -1 1 1 1 0; 0 -1 1 1 1; 0 0 -1 1 1; 0 0 0 -1 1]
