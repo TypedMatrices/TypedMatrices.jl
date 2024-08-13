8bf62358fced255a0c2fa53b17041e1c38a4e74c
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-13 16:34:06 +0100

    test(matrices): add rosser tests

---
+++ test/matrices/rosser.jl
@@ -0,0 +1,14 @@
+# constructors & content (random matrix)
+@test allequal([
+    size(Rosser(4)),
+    size(Rosser(4, 1, 1)),
+    size(Rosser{Float64}(4)),
+    size(Rosser{Float64}(4, 1, 1)),
+])
+@test !isnothing(Rosser(0))
+
+# linear algebra functions
+run_test_linearalgrbra_functions(Rohess.((2) .^ (1:4)))
+
+# eltype
+@test test_matrix_elements(Rohess{Float32}(4))
