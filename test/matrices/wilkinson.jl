42e7f49b67643f812869c58d1ef5fe6b7840324e
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-13 18:41:27 +0100

    test(matrices): add wilkinson tests

---
+++ test/matrices/wilkinson.jl
@@ -0,0 +1,15 @@
+# constructors
+@test allequal([
+    Wilkinson(5),
+    Wilkinson{Float64}(5),
+])
+
+# linear algebra functions
+run_test_linearalgrbra_functions(Wilkinson.(1:5))
+
+# eltype
+@test test_matrix_elements(Wilkinson{Float32}(5))
+
+# content
+@test Wilkinson(5) ≈ [2.0 1.0 0.0 0.0 0.0; 1.0 1.0 1.0 0.0 0.0; 0.0 1.0 0.0 1.0 0.0; 0.0 0.0 1.0 1.0 1.0; 0.0 0.0 0.0 1.0 2.0]
+@test Wilkinson(4) ≈ [1.5 1.0 0.0 0.0; 1.0 0.5 1.0 0.0; 0.0 1.0 0.5 1.0; 0.0 0.0 1.0 1.5]
