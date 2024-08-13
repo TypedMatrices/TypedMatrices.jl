86a504d1739af18aaee71e7e2ed0b48d0edaf701
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-13 18:36:20 +0100

    test(matrices): add triw tests

---
+++ test/matrices/triw.jl
@@ -0,0 +1,20 @@
+# constructors
+@test allequal([
+    Triw(5),
+    Triw(5, -1),
+    Triw(5, -1, 4),
+    Triw{Int}(5),
+    Triw{Int}(5, -1),
+    Triw{Int}(5, -1, 4),
+])
+
+# linear algebra functions
+run_test_linearalgrbra_functions(Triw.(1:5))
+
+# eltype
+@test test_matrix_elements(Triw{Int32}(5))
+
+# content
+@test Triw(5) ≈ [1 -1 -1 -1 -1; 0 1 -1 -1 -1; 0 0 1 -1 -1; 0 0 0 1 -1; 0 0 0 0 1]
+@test Triw(5, 3, 4) ≈ [1 3 3 3 3; 0 1 3 3 3; 0 0 1 3 3; 0 0 0 1 3; 0 0 0 0 1]
+@test Triw(5, 3.2, 2) ≈ [1.0 3.2 3.2 0.0 0.0; 0.0 1.0 3.2 3.2 0.0; 0.0 0.0 1.0 3.2 3.2; 0.0 0.0 0.0 1.0 3.2; 0.0 0.0 0.0 0.0 1.0]
