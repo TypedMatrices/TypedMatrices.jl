e19369ae2320278c44efabf3b8f3d6bae8ad0939
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-13 18:53:17 +0100

    test(matrices): test replace_in_print_matrix on wilkinson

--- test/matrices/wilkinson.jl
+++ test/matrices/wilkinson.jl
@@ -13,3 +13,7 @@ run_test_linearalgrbra_functions(Wilkinson.(1:5))
 # content
 @test Wilkinson(5) ≈ [2.0 1.0 0.0 0.0 0.0; 1.0 1.0 1.0 0.0 0.0; 0.0 1.0 0.0 1.0 0.0; 0.0 0.0 1.0 1.0 1.0; 0.0 0.0 0.0 1.0 2.0]
 @test Wilkinson(4) ≈ [1.5 1.0 0.0 0.0; 1.0 0.5 1.0 0.0; 0.0 1.0 0.5 1.0; 0.0 0.0 1.0 1.5]
+
+# Base.replace_in_print_matrix
+@test Base.replace_in_print_matrix(Wilkinson(5), 1, 1, "a") == "a"
+@test Base.replace_in_print_matrix(Wilkinson(5), 1, 5, "a") != "a"
