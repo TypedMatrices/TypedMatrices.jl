692eb731212ab675d16c087053cb29cf653716c5
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-13 15:26:39 +0100

    test(matrices): add poisson tests

---
+++ test/matrices/poisson.jl
@@ -0,0 +1,14 @@
+# constructors
+@test allequal([
+    Poisson(4),
+    Poisson{Int}(4),
+])
+
+# linear algebra functions
+run_test_linearalgrbra_functions(Poisson.((1:3) .^ 2))
+
+# eltype
+@test test_matrix_elements(Poisson{Int32}(4))
+
+# content
+@test Poisson(4) ≈ [4.0 -1.0 -1.0 0.0; -1.0 4.0 0.0 -1.0; -1.0 0.0 4.0 -1.0; 0.0 -1.0 -1.0 4.0]
