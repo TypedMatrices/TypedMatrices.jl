56d1aaf26c364e0e159be9f0c47562189c9112bf
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-13 12:19:23 +0100

    test(matrices): add lehmer tests

---
+++ test/matrices/lehmer.jl
@@ -0,0 +1,14 @@
+# constructors
+@test allequal([
+    Lehmer(5),
+    Lehmer{Int}(5),
+])
+
+# linear algebra functions
+run_test_linearalgrbra_functions(Lehmer{Float64}.(1:5))
+
+# eltype
+@test test_matrix_elements(Lehmer{Int32}(5))
+
+# content
+@test Lehmer(5) ≈ [1.0 0.5 0.3333333333333333 0.25 0.2; 0.5 1.0 0.6666666666666666 0.5 0.4; 0.3333333333333333 0.6666666666666666 1.0 0.75 0.6; 0.25 0.5 0.75 1.0 0.8; 0.2 0.4 0.6 0.8 1.0]
