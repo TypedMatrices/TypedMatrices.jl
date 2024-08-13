0f955afaeac73737b781c05e39ed4bc70b34f1be
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-13 15:41:11 +0100

    test(matrices): add rohess tests

---
+++ test/matrices/rohess.jl
@@ -0,0 +1,11 @@
+# constructors & content (random matrix)
+@test allequal([
+    size(Rohess(5)),
+    size(Rohess{Float64}(5)),
+])
+
+# linear algebra functions
+run_test_linearalgrbra_functions(Rohess.(1:5))
+
+# eltype
+@test test_matrix_elements(Rohess{Float32}(5))
