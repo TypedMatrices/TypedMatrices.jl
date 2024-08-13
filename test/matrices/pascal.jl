2890893c6a4df7c5de31b322b5b9794c939e0ebb
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-08-13 14:04:40 +0100

    test(matrices): add pascal tests

---
+++ test/matrices/pascal.jl
@@ -0,0 +1,14 @@
+# constructors
+@test allequal([
+    Pascal(5),
+    Pascal{Int}(5),
+])
+
+# linear algebra functions
+run_test_linearalgrbra_functions(Pascal.(1:5))
+
+# eltype
+@test test_matrix_elements(Pascal{Int32}(5))
+
+# content
+@test Pascal(5) ≈ [1 1 1 1 1; 1 2 3 4 5; 1 3 6 10 15; 1 4 10 20 35; 1 5 15 35 70]
