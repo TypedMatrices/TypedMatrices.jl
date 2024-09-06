e4a3eb0d810b2a5efdd28646fa1729e1e1b61305
Massimiliano Fasi <massimiliano.fasi@gmail.com>
2024-09-06 12:34:22 +0100

    fix(matrices): minij properties

--- src/matrices/minij.jl
+++ src/matrices/minij.jl
@@ -26,7 +26,7 @@ end
 Minij(n::Integer) = Minij{Int}(n)
 
 # metadata
-@properties Minij [:symmetric, :inverse, :posdef, :eigen]
+@properties Minij [:symmetric, :inverse, :posdef, :infdiv, :eigen, :integer, :positive]
 
 # properties
 size(A::Minij) = (A.n, A.n)
