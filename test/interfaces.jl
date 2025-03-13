7e1075886e1c628d77434c597170c9b4971e8410
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2025-03-13 23:05:59 +0000

    test(interfaces): change expected error type from ArgumentError to Exception in test_algorithm test

--- test/interfaces.jl
+++ test/interfaces.jl
@@ -8,5 +8,5 @@ end
     @test isa(test_algorithm(sum_elements, [1, 2, 3], ignore_errors=true), Vector)
     @test isa(test_algorithm(sum_elements, [1, 2, 3], errors_as_warnings=true, ignore_errors=true), Vector)
     @test isa(test_algorithm(sum_elements, [1, 2, 3], ignore_errors=true, excludes=[Hilbert]), Vector)
-    @test_throws ArgumentError test_algorithm(sum_elements, [1, 2, 3])
+    @test_throws Exception test_algorithm(sum_elements, [1, 2, 3])
 end
